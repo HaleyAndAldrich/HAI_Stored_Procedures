72119afd78dc2954d971f8a1500f531e622be2fc
HaleyAndAldrich <dhiggins@haleyaldrich.com>
2017-02-27 12:09:06 -0800

    Fix  mthgrp link to cas_rn in results output

--- exec sp_HAI_WQX_Export.sql
+++ exec sp_HAI_WQX_Export.sql
@@ -1,22 +1,23 @@
+use equis
+go
 
-
-declare @facility_id int = (select facility_id from dt_facility where facility_name like '%yak%')
-insert into rt_remap_detail(external_field, external_value, internal_value, status_flag, remap_code, remark)
-	select distinct
-	'WQX_mth'
-	,analytic_method
-	,analytic_method
-	,'A'
-	,'EPA_WQX'
-	,'Haley Aldrich'
-	 from dt_test
-	where facility_id = @facility_id
-	and analytic_method like '%calc%'
-	and analytic_method not in (
-		select
-	internal_value
-	from rt_remap_detail
-	where remap_code = 'EPA_WQX'
-	and external_field = 'WQX_mth')
-
+declare @fac int = (select facility_id from dt_facility where facility_name like '%yakima%')
+exec [rpt].[sp_HAI_WQX_Export]
+	 @fac,
+	  null, --@location_groups varchar (2000),
+	  null, --@locations varchar (2000),
+	  'n', --@sample_type varchar (20),
+	  '2014 fall', --'Fall 2014', --@task_codes varchar (1000),
+	 '1/1/2010', --@start_date datetime,
+	 '1/1/2017', --@end_date datetime,
+	  null, --@mth_grp varchar(2000),
+	  null, --@param varchar (2000),
+	  null, --@fraction varchar(10) = null,
+	  'so', --@matrix_codes varchar (500),
+	  null, --@target_unit varchar(100),
+	  null, --@limit_type varchar (10) = 'RL',
+	  null, --@user_qual_def varchar (10) = '# Q',
+	  'n', --@show_val_yn varchar (10) = 'N',
+	 'Location', --@rpt_flag varchar (20),    -- Determine if exporting WQX_Location, WQX_Result or WQX_Weather
+	  null --@coord_type 
 
