1c14f3f3b6f7b903932e73b1ea401edd63b8f7f6
HaleyAndAldrich <dhiggins@haleyaldrich.com>
2017-03-29 08:14:33 -0700

    update to convert comp_f to comp
    comp_f not ID's in sample plan so modified to ignore comp_f

--- exec sp_hai_discharge_monitoring_report.sql
+++ exec sp_hai_discharge_monitoring_report.sql
@@ -1,4 +1,6 @@
+use equis
 
+go
 
 exec [HAI].[sp_hai_discharge_monitoring_report]
 
@@ -6,8 +8,8 @@ exec [HAI].[sp_hai_discharge_monitoring_report]
 	 ,'2017'  --@event_year varchar (4000)
 	 ,'1st quarter'  --@event_quarter varchar(4000)
 	 ,'1/1/2017'  --@start_date datetime
-	 ,'2/1/2017'  --@end_date datetime
-	 ,'outfall 009'  --@locations varchar (2000)
+	 ,'3/1/2017'  --@end_date datetime
+	 ,'outfall 002'  --@locations varchar (2000)
 	 ,null  --@location_groups varchar (2000)
 	 ,'n'  --@sample_type_code varchar (100)
 	 ,'rl'  --@limit_type varchar (10)
