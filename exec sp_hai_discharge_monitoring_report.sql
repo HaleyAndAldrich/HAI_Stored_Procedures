73e07ec9fdfc8832548383b45182b1eca86a7cef
HaleyAndAldrich <dhiggins@haleyaldrich.com>
2017-04-05 12:33:02 -0700

    updates

--- exec sp_hai_discharge_monitoring_report.sql
+++ exec sp_hai_discharge_monitoring_report.sql
@@ -7,9 +7,9 @@ exec [HAI].[sp_hai_discharge_monitoring_report]
       153 --@facility_id int 
 	 ,'2017'  --@event_year varchar (4000)
 	 ,'1st quarter'  --@event_quarter varchar(4000)
-	 ,'1/1/2017'  --@start_date datetime
-	 ,'3/1/2017'  --@end_date datetime
-	 ,'outfall 002'  --@locations varchar (2000)
+	 ,'1/01/2017'  --@start_date datetime
+	 ,'4/01/2017'  --@end_date datetime
+	 ,'outfall 009'  --@locations varchar (2000)
 	 ,null  --@location_groups varchar (2000)
 	 ,'n'  --@sample_type_code varchar (100)
-	 ,'rl'  --@limit_type varchar (10)
+	 ,'mdl'  --@limit_type varchar (10)
