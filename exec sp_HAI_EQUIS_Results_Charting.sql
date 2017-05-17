978a47f4ada8960242faa43a63ac75b25b7f8547
HaleyAndAldrich <dhiggins@haleyaldrich.com>
2017-05-17 09:45:58 -0700

    update

--- exec sp_HAI_EQUIS_Results_Charting.sql
+++ exec sp_HAI_EQUIS_Results_Charting.sql
@@ -3,9 +3,10 @@ go
 declare @facility_id int = (select facility_id from dt_facility where facility_name like 'stuart%')
 
 exec [HAI].[sp_HAI_EQUIS_Results_Charting]
-	 4464964,
+	 2377479,
+	 'JMSS-Landfill 9', --@subfacilities varchar(2000)
 	 null, --@location_groups varchar (2000),
-	 'MW-12|MW-10|MW-11|MW-3B', --@locations varchar (2000),
+	 null, --'MW-12|MW-10|MW-11|MW-3B', --@locations varchar (2000),
 	 null, --@sample_type varchar (20),
 	 null, --@task_codes varchar (1000),
 	 '1/1/1996', --@start_date datetime, --= 'jan 01 1900 12:00 AM',
