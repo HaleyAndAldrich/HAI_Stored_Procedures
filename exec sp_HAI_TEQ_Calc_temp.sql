578adcb49891a942b76d2ef344c3f594d257f45b
HaleyAndAldrich <dhiggins@haleyaldrich.com>
2018-01-09 15:26:37 -0800

    HAI TEQ Calce
    add parameter groups

--- exec sp_HAI_TEQ_Calc_temp.sql
+++ exec sp_HAI_TEQ_Calc_temp.sql
@@ -5,11 +5,12 @@ exec
 [rpt].[sp_HAI_TEQ_Calc_temp]
 		47 -- @facility_id int
 		,'0' --@ND_mult float 
-		,'se' --@matrix varchar (20)
-		,'EH_SE_2015Jan|eh-sedchar|EH_SE_2015dec|EH_SE_2016mar|EH_SE_2016oct|EH_sea_se_pore_2015'
+		,'wg' --@matrix varchar (20)
+		,'row_well_feasibility_gw'
+		,'PGE-GW-Alk-PAHs-051517'    --@analyte_groups
 		,null --@SDG varchar ( 2000)
-		,'pah_25_sum' --@TEF varchar (200)
-		,'ug/kg'  --@unit varchar (20)
+		,'pah_16_sum' --@TEF varchar (200)
+		,'ug/l'  --@unit varchar (20)
 		,'RL'  --@DL_type varchar (20)
 		,'GW TPAH' --@new_chem_name varchar (255)
 	
